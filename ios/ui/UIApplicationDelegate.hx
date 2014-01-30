3a70bac1c5ebd6124fe58ae527465daf630addf5
Sam Solon <sam.solon@vantos.com>
2014-01-30 11:18:40 -0800

    Haxe compatible cleanup.
    Since we can't have optional methods all the methods will be required.

--- ios/ui/UIApplicationDelegate.hx
+++ ios/ui/UIApplicationDelegate.hx
@@ -1,6 +1,15 @@
 package ios.ui;
 
+import objc.foundation.NSArray;
+import objc.foundation.NSCoder;
+import objc.foundation.NSData;
+import objc.foundation.NSError;
 import objc.foundation.NSDictionary;
+import objc.foundation.NSURL;
+
+import objc.graphics.CGGeometry;
+
+import ios.ui.UIApplication;
 
 @:framework("UIKit")
 extern interface UIApplicationDelegate {
@@ -13,24 +22,33 @@ extern interface UIApplicationDelegate {
 	public function applicationDidBecomeActive (application:UIApplication) :Void;
 	public function applicationWillResignActive (application:UIApplication) :Void;
 
-#if dispaly
+	@:sel("application:handleOpenURL:")
 	public function applicationHandleOpenURL (application:UIApplication, handleOpenURL:NSURL) :Bool;
+	@:sel("application:openURL:sourceApplication:annotation:")
 	public function applicationOpenURL (application:UIApplication, openURL:NSURL, sourceApplication:String, annotation:Dynamic) :Bool;
 
 	public function applicationDidReceiveMemoryWarning (application:UIApplication) :Void;
 	public function applicationWillTerminate (application:UIApplication) :Void;
 	public function applicationSignificantTimeChange (application:UIApplication) :Void;
 
+	@sel("application:willChangeStatusBarOrientation:duration:")
 	public function applicationWillChangeStatusBarOrientation (application:UIApplication, willChangeStatusBarOrientation:UIInterfaceOrientation, duration:Int) :Void;
+	@sel("application:didChangeStatusBarOrientation:")
 	public function applicationDidChangeStatusBarOrientation (application:UIApplication, didChangeStatusBarOrientation:UIInterfaceOrientation) :Void;
 
+	@sel("application:willChangeStatusBarFrame:")
 	public function applicationWillChangeStatusBarFrame (application:UIApplication, willChangeStatusBarFrame:CGRect) :Void;
+	@sel("application:didChangeStatusBarFrame:")
 	public function applicationDidChangeStatusBarFrame (application:UIApplication, didChangeStatusBarFrame:CGRect) :Void;
 
+	@sel("application:didRegisterForRemoteNotificationsWithDeviceToken:")
 	public function applicationDidRegisterForRemoteNotificationsWithDeviceToken (application:UIApplication, didRegisterForRemoteNotificationsWithDeviceToken:NSData) :Void;
+	@sel("application:didFailToRegisterForRemoteNotificationsWithError:")
 	public function applicationDidFailToRegisterForRemoteNotificationsWithError (application:UIApplication, didFailToRegisterForRemoteNotificationsWithError:NSError) :Void;
 
+	@sel("application:didReceiveRemoteNotification:")
 	public function applicationDidReceiveRemoteNotification (application:UIApplication, didReceiveRemoteNotification :NSDictionary) :Void;
+	@sel("application:didReceiveLocalNotification:")
 	public function applicationDidReceiveLocalNotification (application:UIApplication, didReceiveLocalNotification :UILocalNotification) :Void;
 
 	public function applicationDidEnterBackground (application:UIApplication) :Void;
@@ -39,12 +57,20 @@ extern interface UIApplicationDelegate {
 	public function applicationProtectedDataWillBecomeUnavailable (application:UIApplication) :Void;
 	public function applicationProtectedDataDidBecomeAvailable (application:UIApplication) :Void;
 
-	public function applicationSupportedInterfaceOrientationsForWindow (application:UIApplication, supportedInterfaceOrientationsForWindow:UIWindow) :Int;
+//	public function applicationSupportedInterfaceOrientationsForWindow (application:UIApplication, supportedInterfaceOrientationsForWindow:UIWindow) :Int;
 
+	@sel("application:viewControllerWithRestorationIdentifierPath:coder:")
 	public function applicationViewControllerWithRestorationIdentifierPath (application:UIApplication, viewControllerWithRestorationIdentifierPath:NSArray, coder:NSCoder) :UIViewController;
+	
+	@sel("application:shouldSaveApplicationState:")
 	public function applicationShouldSaveApplicationState (application:UIApplication, shouldSaveApplicationState:NSCoder) :Bool;
+	
+	@sel("application:shouldRestoreApplicationState:")
 	public function applicationShouldRestoreApplicationState (application:UIApplication, shouldRestoreApplicationState:NSCoder) :Bool;
+	
+	@sel("application:willEncodeRestorableStateWithCoder:")
 	public function applicationWillEncodeRestorableStateWithCoder (application:UIApplication, willEncodeRestorableStateWithCoder:NSCoder) :Void;
+	
+	@sel("application:didEncodeRestorableStateWithCoder:")
 	public function applicationDidDecodeRestorableStateWithCoder (application:UIApplication, didDecodeRestorableStateWithCoder:NSCoder) :Void;
-#end
 }
